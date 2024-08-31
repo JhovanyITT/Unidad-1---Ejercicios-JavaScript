867c4216f19d2dbf75479eff0d7531440028d6fe
jhova <jhovanygabriel@hotmail.com>
2024-08-30 18:47:13 -0700

    Commit Jhovany Gabriel Jaime Fernandez

---
+++ Parte 1/condicionalesBasicos.js
@@ -0,0 +1,4 @@
+function verificarEdad (a){
+    return a >= 18 ? "Eres mayor de edad":"No eres mayor de edad";
+ }
+ console.log(verificarEdad(18))
---
+++ Parte 1/numeros.js
@@ -0,0 +1,8 @@
+let a = 12;
+let b = 4;
+
+//operaciones
+console.log(a+b);
+console.log(a-b);
+console.log(a*b);
+console.log(a/b);
